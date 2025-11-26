a2076ef95dff3dc8620e84435d928a6dd1ede83d
d44 <j.ares.8888@gmail.com>
2025-11-25 19:53:09 -0500

    skeleton

--- task.go
+++ task.go
@@ -3,23 +3,41 @@ package main
 import (
 	"os"
 	"fmt"
+	"time"
+	"encoding/json"
+	"strings"
 )
 
 type task struct {
 	id int
 	desc string
 	status string
-	created string
-	updated string
+	created time.Time
+	updated time.Time
 }
 
 func main() {
 	var args []string = os.Args
 	amnt := len(args)
-	if amnt != 2 {
+	if amnt != 2 || args != 3 {
 		fmt.Println("invalid number of arguments")
 		return
 	}
 	fmt.Println(args[1])
+
+	switch args[1] {
+		case "add":
+			//
+		case "update":
+			//
+		case "delete":
+			//
+		case "mark":
+			//
+		case "done":
+			//
+		case "list":
+			//
+	}
 }
 
